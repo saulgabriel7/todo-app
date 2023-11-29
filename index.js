9538e6f20394bbfd12b0140e30cee0d4a838c019
Saul Gabriel <saulgabriel069@gmail.com>
2023-11-28 22:26:26 -0300

    salvando uma tarefa no banco de dados

--- index.js
+++ index.js
@@ -9,6 +9,31 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+//converter dados do formulario em objetos JS
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+//rotas
+app.post('/criar', (requisicao, resposta) =>{
+    const descricao = requisicao.body.descricao
+    const completa = 0
+    const sql = `
+        INSERT INTO tarefas(descricao, completa)
+        VALUES ('${descricao}','${completa}')   
+    `
+
+    conexao.query(sql, (erro)=>{
+        if (erro)
+        return console.log(erro)
+    else {
+        resposta.redirect('/')
+    }
+    })
+})
+
 app.get('/', (requisicao, resposta) =>{
     resposta.render('home')
 })
