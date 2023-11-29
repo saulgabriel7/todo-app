08b973b1db9ba105247a9585df0fca2347eef792
Saul Gabriel <saulgabriel069@gmail.com>
2023-11-28 23:20:08 -0300

    Filtrando as tarefas ativas

--- index.js
+++ index.js
@@ -34,6 +34,21 @@ app.post('/completar', (requisicao, resposta) =>{
     })
 }) 
 
+app.post('/descompletar', (requisicao, resposta) => {
+    const id = requisicao.body.id
+    const sql = `
+    UPDATE tarefas
+    SET completa = '0'
+    WHERE id = ${id}
+    `
+    conexao.query(sql, (erro) => {
+        if(erro){
+            return console.log(erro)
+        }
+        resposta.redirect('/')
+        })
+})
+
 app.post('/criar', (requisicao, resposta) =>{
     const descricao = requisicao.body.descricao
     const completa = 0
@@ -65,8 +80,13 @@ app.get('/', (requisicao, resposta) =>{
                     completa: dado.completa === 0 ? false : true
                 }
             })
+            const tarefasAtivas = tarefas.filter((tarefa)=>{
+                return tarefa.completa === false && tarefa
+            })
 
-            resposta.render('home', { tarefas })
+            const quantidadeTarefasAtivas = tarefasAtivas.length
+
+            resposta.render('home', { tarefas, quantidadeTarefasAtivas })
     })
 })
 
